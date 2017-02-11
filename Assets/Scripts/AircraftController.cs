0ceb2d1ff25adb7abd676873ac5baead943d2cf6
Back2RL <leonardoertelt@gmail.de>
2017-02-11 15:16:27 +0100

    cleaned aircraft controller and added first test for gravity countermeasure

--- Assets/Scripts/AircraftController.cs
+++ Assets/Scripts/AircraftController.cs
@@ -6,70 +6,78 @@ using UnityEngine.UI;
 
 public class AircraftController : MonoBehaviour
 {
+    // Camera
+    public Camera camera;
+    private Vector3 _screenCenter;
 
-    private float thrustPercentage;
-
-
-    public float rollSensitivity = 0.5f;
-    public float pitchSensitivity = 2.0f;
-    public float pitchExponent = 1.5f;
-
+    // Controls
     public bool inputEnabled = true;
+    public float switchToInvertedControlsAngle = 135.0f;
+    private float _precalculatedAngle;
+    public float gravityReductionFactor = 0.001f;
 
+    // Aerodynamics
+    private Rigidbody _aircraft;
+
+    // Engine
+    public GameObject[] thrusters;
     public float maxThrust = 10000.0f;
-    public float thrustBlendSpeed = 0.25f; // how fast thrust increases/decreases
+    public float thrustBlendSpeed = 0.25f; // how fast thrust increases/decreases (default: 4 seconds to max thrust)
+    private float _engineThrottle;
 
-    public Rigidbody aircraft;
-
+    // Wings
     public GameObject LeftWing;
     public GameObject RightWing;
+
+    // Elevator
     public GameObject Elevator;
+    public float pitchExponent = 1.5f;
+
+    // Rudder
     public GameObject Rudder;
-
-    public Camera camera;
-    public float upsideDownFlightAngle = 135.0f;
-
-    public GameObject[] thrusters;
+    public float yawExponent = 1.5f;
 
     // UI
     public Text thrustInputText;
     public Text currentForwardSpeedText;
 
-    private Vector3 screenCenter;
 
     // Use this for initialization
     void Start()
     {
-        aircraft = GetComponent<Rigidbody>();
-        screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f);
+        _aircraft = GetComponent<Rigidbody>();
+        _screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f);
+        _precalculatedAngle = Mathf.Cos(switchToInvertedControlsAngle / 180.0f * Mathf.PI);
     }
 
     void Update()
     {
-        thrustInputText.text = "Thrust = " + thrustPercentage.ToString();
-        Vector3 forwardVel = Vector3.Project(aircraft.velocity, aircraft.transform.forward);
-        currentForwardSpeedText.text = "Speed = " + forwardVel.magnitude.ToString();
+        thrustInputText.text = "Thrust = " + _engineThrottle.ToString();
+        Vector3 forwardVel = Vector3.Project(_aircraft.velocity, _aircraft.transform.forward);
+        currentForwardSpeedText.text = "Speed = " + (forwardVel.magnitude * 3.6f).ToString() + " kph";
     }
 
     void FixedUpdate()
     {
-        Vector3 currVel = aircraft.velocity; // current Velocity in Worldspace
+        Vector3 currVel = _aircraft.velocity; // current Velocity in Globalspace
 
         if (inputEnabled)
         {
-
-
             Vector3 mousePosition = Input.mousePosition;
-            Vector3 mouseDirection = mousePosition - screenCenter;
+            Vector3 mouseDirection = mousePosition - _screenCenter;
 
-            mouseDirection.x = Mathf.Clamp(mouseDirection.x / screenCenter.x, -1.0f, 1.0f);
-            mouseDirection.y = Mathf.Clamp(mouseDirection.y / screenCenter.y, -1.0f, 1.0f);
+            mouseDirection.x = Mathf.Clamp(mouseDirection.x / _screenCenter.x, -1.0f, 1.0f);
+            mouseDirection.y = Mathf.Clamp(mouseDirection.y / _screenCenter.y, -1.0f, 1.0f);
 
             mouseDirection.z = 1.0f;
 
             Vector3 targetDirection = camera.transform.TransformDirection(mouseDirection);
 
             Debug.DrawRay(camera.transform.position, targetDirection * 1000, Color.blue);
+
+            // gravity reduction
+            targetDirection -= Physics.gravity * gravityReductionFactor;
+
 
             targetDirection.Normalize();
 
@@ -78,18 +86,18 @@ public class AircraftController : MonoBehaviour
             float dotTargetMov = Vector3.Dot(movDir, targetDirection);
 
             Debug.DrawRay(camera.transform.position, movDir * 1000, Color.green);
-            Debug.DrawRay(camera.transform.position, aircraft.transform.forward * 1000, Color.red);
+            Debug.DrawRay(camera.transform.position, _aircraft.transform.forward * 1000, Color.red);
 
-            float dotTargetAircraftUp = Vector3.Dot(aircraft.transform.up, targetDirection);
+            float dotTargetAircraftUp = Vector3.Dot(_aircraft.transform.up, targetDirection);
 
             float dotTargetAircraftRight = 0;
-            if (dotTargetAircraftUp < Mathf.Cos(upsideDownFlightAngle / 180.0f * Mathf.PI))
+            if (dotTargetAircraftUp < _precalculatedAngle)
             {
-                dotTargetAircraftRight = -Vector3.Dot(aircraft.transform.right, targetDirection);
+                dotTargetAircraftRight = -Vector3.Dot(_aircraft.transform.right, targetDirection);
             }
             else
             {
-                dotTargetAircraftRight = Vector3.Dot(aircraft.transform.right, targetDirection);
+                dotTargetAircraftRight = Vector3.Dot(_aircraft.transform.right, targetDirection);
             }
 
 
@@ -101,39 +109,36 @@ public class AircraftController : MonoBehaviour
                 roll = -Input.GetAxis("Horizontal");
             }
 
-            float yaw = dotTargetAircraftRight;
+            float yaw = Mathf.Sign(dotTargetAircraftRight) * Mathf.Pow(Mathf.Abs(dotTargetAircraftRight), yawExponent);
 
 
-            Elevator.transform.rotation = Quaternion.LookRotation(aircraft.transform.forward + aircraft.transform.up * pitch, aircraft.transform.up + aircraft.transform.forward * pitch);
-            LeftWing.transform.localRotation = Quaternion.LookRotation(new Vector3(0, 1, 0) + new Vector3(0, 0, 1) * roll, new Vector3(0, 0, -1) + new Vector3(0, 1, 0) * roll);
-            RightWing.transform.localRotation = Quaternion.LookRotation(new Vector3(0, 1, 0) + new Vector3(0, 0, 1) * -roll, new Vector3(0, 0, -1) + new Vector3(0, 1, 0) * -roll);
+            Elevator.transform.rotation =
+                Quaternion.LookRotation(_aircraft.transform.forward + _aircraft.transform.up * pitch,
+                    _aircraft.transform.up + _aircraft.transform.forward * pitch);
+            LeftWing.transform.localRotation =
+                Quaternion.LookRotation(new Vector3(0, 1, 0) + new Vector3(0, 0, 1) * roll,
+                    new Vector3(0, 0, -1) + new Vector3(0, 1, 0) * roll);
+            RightWing.transform.localRotation =
+                Quaternion.LookRotation(new Vector3(0, 1, 0) + new Vector3(0, 0, 1) * -roll,
+                    new Vector3(0, 0, -1) + new Vector3(0, 1, 0) * -roll);
 
-            Rudder.transform.localRotation = Quaternion.LookRotation(new Vector3(0, 0, 1) + new Vector3(1, 0, 0) * -yaw, new Vector3(-1, 0, 0));
+            Rudder.transform.localRotation = Quaternion.LookRotation(
+                new Vector3(0, 0, 1) + new Vector3(1, 0, 0) * -yaw, new Vector3(-1, 0, 0));
             // Thrusters
             float thrustDelta = Input.GetAxis("Vertical") * Time.fixedDeltaTime * thrustBlendSpeed;
-            thrustPercentage = Mathf.Clamp(thrustPercentage + thrustDelta, 0, 1);
+            _engineThrottle = Mathf.Clamp(_engineThrottle + thrustDelta, 0, 1);
 
             foreach (GameObject thruster in thrusters)
             {
-                aircraft.AddForceAtPosition(thruster.transform.forward * maxThrust * thrustPercentage,
+                _aircraft.AddForceAtPosition(thruster.transform.forward * maxThrust * _engineThrottle,
                     thruster.transform.position, ForceMode.Force);
             }
-
-
-
-
-
-            //    LeftWing.transform.Rotate(rollSensitivity * -Input.GetAxis("Mouse X"), 0, 0, Space.Self);
-            //    RightWing.transform.Rotate(rollSensitivity * Input.GetAxis("Mouse X"), 0, 0, Space.Self);
-
-            //    Elevator.transform.Rotate(pitchSensitivity * -Input.GetAxis("Mouse Y"), 0, 0, Space.Self);
-            //
         }
     }
 
     void Awake()
     {
         foreach (WheelCollider w in GetComponentsInChildren<WheelCollider>())
-            w.motorTorque = 0.000001f;
+            w.motorTorque = 0.0000001f;
     }
 }
