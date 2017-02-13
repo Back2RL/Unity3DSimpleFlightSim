d76c55032a41ae3f17c5ad5693ab53fdbc5fdfc7
Back2RL <leonardoertelt@gmail.de>
2017-02-13 22:56:53 +0100

    import tackle

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -18,7 +18,7 @@ public class MainCamera : MonoBehaviour {
 		moveCamTo = Plane.transform.position - Plane.transform.forward * 40.0f + Vector3.up * 2.0f;
 		//float bias = 0.9f;
 		//Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
-		Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, moveCamTo, .1f);
+		Camera.main.transform.position = Vector3.Slerp(Camera.main.transform.position, moveCamTo, 0.5f);
         //transform.LookAt(Plane.transform.position, transform.up);
         transform.rotation = Quaternion.LookRotation(Plane.transform.forward, transform.up);
         //cameraRotation = Vector3.Angle (boxPhysics.localPlaneSpeedProjection, flightProperties.pointer);
